cbde2b207e046df0c948eddd4a73847d41b16aa8
Your Name <jeremyswanson2@gmail.com>
2025-08-15 02:07:48 -0400

    updated pages

--- Frontend/js/vanta-init.js
+++ Frontend/js/vanta-init.js
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
         scaleMobile: 1.00,
         color: 0x29fa1e,
         backgroundColor: 0x000000,
-        points: 10.0,
+        points: 15.0,
         maxDistance: 20.0,
-        spacing: 15.0,
-        showDots: true
+        spacing: 18.0,
+        showDots: false
     });
 });
